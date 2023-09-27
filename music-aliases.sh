47fd4660f6e26941b034d8fd12f10158130babe6
Andrew Combs <andrew.combs@geappliances.com>
2023-09-27 09:43:47 -0400

    Add volume control

--- music-aliases.sh
+++ music-aliases.sh
@@ -13,3 +13,16 @@ spot() {
    start_spotify
    /bin/bash ~/music-scripts/Mustream/play "$@"
 }
+
+# This function created with the assistance of Chat GPT3.5
+# https://chat.openai.com/share/efe6d30b-4b44-4e75-8d9a-90e4eb25c825
+vol() {
+   local input="$1"
+
+   if (( $(echo "$input < 1" | bc -l) )); then
+      playerctl -p spotify volume "$input"
+   else
+      input=$(echo "scale=2; $input / 100" | bc -l)
+      playerctl -p spotify volume "$input"
+   fi
+}
